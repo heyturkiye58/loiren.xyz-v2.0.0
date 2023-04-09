c309941afc030837d898c2b066f3fb3cedede745
Hey Türkiye <110188917+heyturkiye58@users.noreply.github.com>
2023-04-09 13:59:26 +0300

    Update repos.tsx

--- pages/api/util/repos.tsx
+++ pages/api/util/repos.tsx
@@ -10,7 +10,7 @@ export default async function User(req, res) {
         
         let { data: ReposLoiren } = await axios.get('https://api.github.com/users/feignwiki/repos');
         
-        res.send([...ReposLoiren, ...ReposSerity])
+        res.send([...ReposLoiren])
     } catch(err) {
         res.status(500).json({ error: 'Something went wrong!' });
     }
