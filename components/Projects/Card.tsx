c3bd9d2a3784d619bb868fe68216820080e5a3fc
Hey Türkiye <110188917+heyturkiye58@users.noreply.github.com>
2023-04-09 14:07:47 +0300

    Update Card.tsx

--- components/Projects/Card.tsx
+++ components/Projects/Card.tsx
@@ -2,8 +2,8 @@ export default function Projects() {
     return (
         <>
 <a target="_blank" href="https://awardbot.net/" className="mt-6">
- <div style={{ '--card-banner': 'url(https://cdn.discordapp.com/attachments/1079387334795083897/1088570434330562701/awardbot_png.png)' } as any } className="group mt-6 bot-card p-8 relative rounded-lg overflow-hidden transition-all duration-200 w-full">
-            <img className="w-20 h-20 rounded-full -ml-5" src={"https://cdn.discordapp.com/attachments/1079387334795083897/1088570434330562701/awardbot_png.png"} />
+ <div style={{ '--card-banner': 'url(https://cdn.discordapp.com/attachments/971086132097978410/1094574096232239115/BAnner.png)' } as any } className="group mt-6 bot-card p-8 relative rounded-lg overflow-hidden transition-all duration-200 w-full">
+            <img className="w-20 h-20 rounded-full -ml-5" src={"https://avatars.githubusercontent.com/u/94870865?s=200&v=4"} />
             <div className="flex flex-col mt-3 -ml-2">
                 <a className="flex items-center space-x-3 -ml-2">
                     <h1 className="text-zinc-100 leading-none text-2xl">awardbot.net</h1>
