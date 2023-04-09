205fc36091303409d58aaa7efd10bb4f28911002
Hey Türkiye <110188917+heyturkiye58@users.noreply.github.com>
2023-04-09 13:54:36 +0300

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,7 +11,7 @@ export default function About() {
                 <p className="font-medium text-lg text-white/50 text-center">Learn more about me.</p>
             </div>
             <div className="flex text-center text-gray-400/90 font-medium">
-                <h1 className="w-1/2 mx-auto">My name is Berk. I was born in Turkey and I continue to live. I am high school student. I am passionate about programming.</h1>
+                <h1 className="w-1/2 mx-auto">coming soon...</h1>
             </div>
         </>
     );
