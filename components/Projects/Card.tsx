53f775aefacfdb4c8ffdb5bcd2ed4e41a6247ef2
Hey Türkiye <110188917+heyturkiye58@users.noreply.github.com>
2023-04-09 13:43:55 +0300

    Update Card.tsx

--- components/Projects/Card.tsx
+++ components/Projects/Card.tsx
@@ -1,61 +1,29 @@
 export default function Projects() {
     return (
         <>
-<a target="_blank" href="https://daisybot.tech" className="mt-6">
- <div style={{ '--card-banner': 'url(https://i.hizliresim.com/d8x6b2d.png)' } as any } className="group mt-6 bot-card p-8 relative rounded-lg overflow-hidden transition-all duration-200 w-full">
-            <img className="w-20 h-20 rounded-full -ml-5" src={"https://cdn.discordapp.com/avatars/967357549353500713/1261b01a8ab38fac0e2e5ce71bd99741.png?size=4096"} />
+<a target="_blank" href="https://awardbot.net/" className="mt-6">
+ <div style={{ '--card-banner': 'url(https://cdn.discordapp.com/attachments/1079387334795083897/1088570434330562701/awardbot_png.png)' } as any } className="group mt-6 bot-card p-8 relative rounded-lg overflow-hidden transition-all duration-200 w-full">
+            <img className="w-20 h-20 rounded-full -ml-5" src={"https://cdn.discordapp.com/attachments/1079387334795083897/1088570434330562701/awardbot_png.png"} />
             <div className="flex flex-col mt-3 -ml-2">
                 <a className="flex items-center space-x-3 -ml-2">
-                    <h1 className="text-zinc-100 leading-none text-2xl">daisybot.tech</h1>
+                    <h1 className="text-zinc-100 leading-none text-2xl">awardbot.net</h1>
                 </a>
-                <h6 className="font-medium mt-1 text-white/40 text-sm -ml-2">Compete with your friends, compete with companies, play games with Daisy</h6>
+                <h6 className="font-medium mt-1 text-white/40 text-sm -ml-2">Create the best giveaways with Award!</h6>
             </div>
         </div>
     </a>
-    <a target="_blank" href="https://serity.me" className="mt-6">
- <div style={{ '--card-banner': 'url(https://i.ibb.co/hZXc0p0/seritybanner.png)' } as any } className="group mt-6 bot-card p-8 relative rounded-lg overflow-hidden transition-all duration-200 w-full">
-            <img className="w-20 h-20 rounded-full -ml-5" src={"https://cdn.discordapp.com/icons/701153938870894673/a567eb70003eb1c502894ac4408d5547.png?size=4096"} />
+    <a target="_blank" href="https://feign-wiki.tk/" className="mt-6">
+ <div style={{ '--card-banner': 'url(https://feign-wiki.tk/favicon.ico)' } as any } className="group mt-6 bot-card p-8 relative rounded-lg overflow-hidden transition-all duration-200 w-full">
+            <img className="w-20 h-20 rounded-full -ml-5" src={"https://feign-wiki.tk/favicon.ico"} />
             <div className="flex flex-col mt-3 -ml-2">
                 <a className="flex items-center space-x-3 -ml-2">
-                    <h1 className="text-zinc-100 leading-none text-2xl">Serity</h1>
+                    <h1 className="text-zinc-100 leading-none text-2xl">Feign Wiki</h1>
                 </a>
-                <h6 className="font-medium mt-1 text-white/40 text-sm -ml-2">Create your own unique website with Serity! Licensed and easy-to-install products!</h6>
+                <h6 className="font-medium mt-1 text-white/40 text-sm -ml-2">Feign oyunu hakkında Miraheze Limited tarafından barındırılmış vikidir.</h6>
             </div>
         </div>
     </a>
-    <a target="_blank" href="https://github.com/templatet/" className="mt-6">
- <div style={{ '--card-banner': 'url(https://cdn.discordapp.com/attachments/915331943875358731/962050065470459974/unknown.png)' } as any } className="group mt-6 bot-card p-8 relative rounded-lg overflow-hidden transition-all duration-200 w-full">
-            <img className="w-20 h-20 rounded-full -ml-5" src={"https://avatars.githubusercontent.com/u/103275381?s=400&u=c3a74a230d0bfa6d30ebee09ffbb76ef4291a42e&v=4"} />
-            <div className="flex flex-col mt-3 -ml-2">
-                <a className="flex items-center space-x-3 -ml-2">
-                    <h1 className="text-zinc-100 leading-none text-2xl">Templatet</h1>
-                </a>
-                <h6 className="font-medium mt-1 text-white/40 text-sm -ml-2">Templatet is an open source project with templates that you can use. {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'}{'\u00A0'} {'\u00A0'} {'\u00A0'} </h6>
-            </div>
-        </div>
-    </a>
-        <a target="_blank" href="https://discord.com/api/oauth2/authorize?client_id=953024785292607538&permissions=8&scope=bot%20applications.commands" className="mt-6">
-         <div style={{ '--card-banner': 'url(https://wallpaperaccess.com/full/3053366.png)' } as any } className="group mt-6 bot-card p-8 relative rounded-lg overflow-hidden transition-all duration-200 w-full">
-            <img className="w-20 h-20 rounded-full -ml-5" src={`https://cdn.discordapp.com/avatars/953024785292607538/9c3d2099130296964d615ea142b09747.png?size=4096`} />
-            <div className="flex flex-col mt-3 -ml-2">
-                <a className="flex items-center space-x-3 -ml-2">
-                    <h1 className="text-zinc-100 leading-none text-2xl">Ticketcord</h1>
-                </a>
-                <h6 className="font-medium mt-1 text-white/40 text-sm -ml-2">Set up a ticket system on your server with Ticketcord. {'\u00A0'} {'\u00A0'} {'\u00A0'}{'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'}</h6>
-            </div>
-        </div>
-    </a>
-    <a target="_blank" href="https://partnerbot.xyz" className="mt-6">
-         <div style={{ '--card-banner': 'url(https://i.imgur.com/5ycEQXC.png)' } as any } className="group mt-6 bot-card p-8 relative rounded-lg overflow-hidden transition-all duration-200 w-full">
-            <img className="w-20 h-20 rounded-full -ml-5" src={`https://media.discordapp.net/attachments/969966773308887081/1030574473839120485/147135b25a0d0fcc409dba6355fead8aa.png?width=580&height=580`} />
-            <div className="flex flex-col mt-3 -ml-2">
-                <a className="flex items-center space-x-3 -ml-2">
-                    <h1 className="text-zinc-100 leading-none text-2xl">Dark Partner</h1>
-                </a>
-                <h6 className="font-medium mt-1 text-white/40 text-sm -ml-2">Experience the advanced Discord, find your server partner now and watch you grow.  {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} {'\u00A0'} </h6>
-            </div>
-        </div>
-    </a>
+    
         </>
     );
 };
